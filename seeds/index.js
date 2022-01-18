5987e3467f30cd7f89ccea6e82da3c359c266931
Darko <89515851+DarkoPG27@users.noreply.github.com>
2022-01-18 15:23:39 +0100

    Adding Images

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,14 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`, //lokacija
-            title: `${sample(descriptors)} ${sample(places)} ` // random ime kampa
+            title: `${sample(descriptors)} ${sample(places)} `, // random ime kampa
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis consectetur vero unde perspiciatis ipsum natus possimus, consequatur id necessitatibus omnis reprehenderit rerum aliquid nesciunt deserunt consequuntur error soluta officia? Et!',
+            price
+
         })
         await camp.save();
     }
