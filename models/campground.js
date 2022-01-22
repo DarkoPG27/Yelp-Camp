ed44440a660777434b9b5ecbb083e65feb55300d
Darko <89515851+DarkoPG27@users.noreply.github.com>
2022-01-22 14:37:34 +0100

    Added a Thumbnail Virtual Property

--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,19 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+// https://res.cloudinary.com/demo/image/upload/w_150,h_300,c_fill/boulder.jpg
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200'); //replace only first match
+});
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
