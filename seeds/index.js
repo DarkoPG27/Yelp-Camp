79cea3c99f13d65bba682834b9508a3fd2ad3049
Darko <89515851+DarkoPG27@users.noreply.github.com>
2022-01-24 12:46:05 +0100

    Fixing seeds bug

--- seeds/index.js
+++ seeds/index.js
@@ -19,22 +19,26 @@ const sample = array => array[Math.floor(Math.random() * array.length)]
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 5; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '61e942e53a636c00123b348f',
+            author: '61e942e53a636c00123b348f',//USER ID
             location: `${cities[random1000].city}, ${cities[random1000].state}`, //lokacija
             title: `${sample(descriptors)} ${sample(places)} `, // random ime kampa
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis consectetur vero unde perspiciatis ipsum natus possimus, consequatur id necessitatibus omnis reprehenderit rerum aliquid nesciunt deserunt consequuntur error soluta officia? Et!',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [19.26611, 42.43972]
+            },
             images: [
                 {
-                    url: 'https://res.cloudinary.com/duumzqdiz/image/upload/v1642838517/YelpCamp/wztldvrlpzfqplbv5pog.jpg',
+                    url: 'https://res.cloudinary.com/duumzqdiz/image/upload/v1643024484/YelpCamp/litrgxvsupehgy0debcd.jpg',
                     filename: 'YelpCamp/wztldvrlpzfqplbv5pog',
                 },
                 {
-                    url: 'https://res.cloudinary.com/duumzqdiz/image/upload/v1642838519/YelpCamp/khe0qjop4ss1nkeu6b2p.jpg',
+                    url: 'https://res.cloudinary.com/duumzqdiz/image/upload/v1643024484/YelpCamp/zynyibpglsm89hjv0izv.jpg',
                     filename: 'YelpCamp/khe0qjop4ss1nkeu6b2p',
                 }
             ]
