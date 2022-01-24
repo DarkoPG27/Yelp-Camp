92842f11b132109084356f39067e39236a7b5c0b
Darko <89515851+DarkoPG27@users.noreply.github.com>
2022-01-24 12:18:31 +0100

    Centering the Map on a Campground

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,14 @@
 mapboxgl.accessToken = mapToken;
+
 const map = new mapboxgl.Map({
     container: 'map',
-    center: [-74.5, 40],
+    center: camp, // ****
     zoom: 9,
     style: 'mapbox://styles/mapbox/streets-v11'
 });
+console.log(camp)
+// default Marker, colored black, rotated 45 degrees. new mapboxgl.Marker({ color: 'red', rotation: 45 })
+
+const marker1 = new mapboxgl.Marker()
+    .setLngLat(camp) //  ****
+    .addTo(map); 
